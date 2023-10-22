b3f704918582d0367a3a2110b5d565c7c23a8647
Vanshul Agarwal <vanshulagarwal2004@gmail.com>
2023-10-22 20:04:30 +0530

    completed frontend

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/home', async (req, res) => {
     const allLists = await List.find({}).populate('items');

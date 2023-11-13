3e2fec8f540b61df3d11932c77c548ba41aee4cb
Vanshul Agarwal <vanshulagarwal2004@gmail.com>
2023-11-14 02:22:20 +0530

    resolved security issues

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -8,8 +12,11 @@ const itemRoutes = require('./routes/items');
 const userRoutes = require('./routes/users');
 const session = require('express-session');
 const flash = require('connect-flash');
+const MongoStore = require("connect-mongo");
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 
-const dbUrl = 'mongodb://127.0.0.1:27017/todo';
+const dbUrl = process.env.ATLAS_URL || 'mongodb://127.0.0.1:27017/todo';
 mongoose.connect(dbUrl)
     .then(() => {
         console.log('mongo database connected');
@@ -23,15 +30,36 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize({
+    replaceWith: '_'
+}))
+
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!'
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret: secret
+    }
+});
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+})
 
 //creating a session
 const sessionConfig = {
-    secret: 'thisshouldbeabettersecret!',
+    store,
+    secret: secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
+        name: 'session',
         httpOnly: true,
+        // secure: true, //this line makes sure that only https and not http has access to our cookies
+        // the problem is that localhost is also http, therefore we won't be able to login or use any other cookie on localhost
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         //auto deletes after 7 days
         maxAge: 1000 * 60 * 60 * 24 * 7
@@ -39,9 +67,47 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdnjs.cloudflare.com/",
+];
+const connectSrcUrls = [
+];
+const fontSrcUrls = [
+    "https://cdnjs.cloudflare.com/",
+    "https://fonts.gstatic.com",
+];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 app.use((req, res, next) => {
-    res.locals.user_id=req.session.user_id;
+    res.locals.user_id = req.session.user_id;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     res.locals.caution = req.flash('caution');
--- schemas.js
+++ schemas.js
@@ -1,11 +1,34 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHTML = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHTML(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean != value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+})
+
+const Joi=BaseJoi.extend(extension);
 
 module.exports.listSchema = Joi.object({
-    title: Joi.string().required().min(1).max(12),
+    title: Joi.string().required().min(1).max(12).escapeHTML(),
     items: Joi.array()
 });
 
 module.exports.itemSchema = Joi.object({
-    task: Joi.string().required().min(1).max(25),
+    task: Joi.string().required().min(1).max(25).escapeHTML(),
     completed: Joi.boolean()
 });
